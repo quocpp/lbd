da5ef543da65151aebfd1b5d823d42e44188f8c1
quocpp <quocpp@hotmail.com>
2013-12-09 16:19:10 +0700

    first commit

---
+++ App/src/main/java/com/LBD/app/MainActivity.java
@@ -0,0 +1,102 @@
+package com.LBD.app;
+
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+        Log.d("BEAN","START");
+
+        new getHtml().execute("http://www.livescore.com/soccer/england/premier-league/fixtures");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+    private class getHtml extends AsyncTask<String,Void,Void>
+    {
+        @Override
+        protected Void doInBackground(String... urls)
+        {
+            Document doc = null;
+            try
+            {
+                ArrayList<ArrayList<String>> lbdTable = new ArrayList<ArrayList<String>>();
+                doc = Jsoup.connect(urls[0]).get();
+                Elements table = doc.getElementsByClass("mtn");
+                Document doc1 = Jsoup.parse(table.toString());
+                Elements even = doc1.getElementsByClass("even");
+                Log.d("BEAN",table.toString());
+                Log.d("BEAN","DONE");
+            }
+            catch (Exception e)
+            {
+                Log.d("BEAN",e.toString());
+            }
+            return null;
+        }
+    }
+}
+
+
